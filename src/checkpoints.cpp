39968e3d40ce8e6b61bb9d2ef335b96f60db9373
terracoin <terracoin76@gmail.com>
2013-04-17 13:32:25 +0200

    [checkpoints] another checkpoint...

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -89,11 +89,12 @@ namespace Checkpoints
        (106753, uint256("0x0000000000003cb9976d3785c2960728bdd67d4ddfce640682b9c9c4df8582d9"))
        (106759, uint256("0x000000000002ed231df0f8c4f21f2a73eeacbbc88f411b438ad4fd4b60418c42"))
        (107368, uint256("0x000000000009062fe1b4c0654b3d152282545aa8beba3c0e4981d9dfa71b1eaa"))
+       (108106, uint256("0x00000000000282bf4e2bd0571c42165a67ffede3b81f2387e301369162107020"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1366126914, // * UNIX timestamp of last checkpoint block
-        203494,     // * total number of transactions between genesis and last checkpoint
+        1366189767, // * UNIX timestamp of last checkpoint block
+        205310,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         2500.0      // * estimated number of transactions per day after checkpoint
     };
